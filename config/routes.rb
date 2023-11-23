10db0aa83051b9c1f5ea5e73a34a2edd2fe752ca
luvemanates <luvemanates@gmail.com>
2023-11-23 14:42:07 -0800

    fixed ticket number output and removed old tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users
+  #devise_for :users
 
-  devise_for :users
-
-  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   resources :tickets do 
     put "bcc", :action => 'bcc'
