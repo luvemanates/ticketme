f9f7ea45575b95b0ef1392afa78dcf7f2b6c4457
luvemanates <luvemanates@gmail.com>
2024-01-04 16:55:04 -0800

    the paginate var should be at the top anyway - but it created a bug at the bottom where it stopped working

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
+WillPaginate.per_page = 5
 class Ticket < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
 end
-WillPaginate.per_page = 5
