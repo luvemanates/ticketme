7123ac5676dc2ca076765ab1d0f151057ad14b7a
luvemanates <luvemanates@gmail.com>
2024-01-19 09:32:51 -0800

    moved ticket color selector to helper

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,9 @@
 module TicketsHelper
+  def ticket_color(current_ticket_count)
+    current_ticket_count = 1 unless current_ticket_count 
+    color_selector = current_ticket_count % 4 
+    ticket_colors = ["blue", "red", "green", "yellow"] 
+    ticket_color = ticket_colors[color_selector] 
+    return ticket_color
+  end
 end
