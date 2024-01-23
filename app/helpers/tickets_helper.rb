36e2ad43fdfe832b0f5c5cf74473cd9874829985
luvemanates <luvemanates@gmail.com>
2024-01-23 15:08:55 -0800

    more styling for a more professional look (#2)

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -6,4 +6,12 @@ module TicketsHelper
     ticket_color = ticket_colors[color_selector] 
     return ticket_color
   end
+
+  def ticket_tape_version(current_ticket_count)
+    current_ticket_count = 1 unless current_ticket_count 
+    tape_selector = current_ticket_count % 2
+    ticket_tape = ["middle", "top_left"] 
+    ticket_tape_select = ticket_tape[tape_selector] 
+    return ticket_tape_select
+  end
 end
