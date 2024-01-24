3b7616ec331fc9894013a0905873a45a670cf175
luvemanates <luvemanates@gmail.com>
2024-01-24 10:55:08 -0800

    fixed search styling and added tape to right corner for some tickets also positioned ticket buttons (#2)

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -9,8 +9,8 @@ module TicketsHelper
 
   def ticket_tape_version(current_ticket_count)
     current_ticket_count = 1 unless current_ticket_count 
-    tape_selector = current_ticket_count % 2
-    ticket_tape = ["middle", "top_left"] 
+    tape_selector = current_ticket_count % 3 
+    ticket_tape = ["top_left", "middle", "top_right"] 
     ticket_tape_select = ticket_tape[tape_selector] 
     return ticket_tape_select
   end
