26464ea4131ae808b15d92267fb5d30b22750e66
luvemanates <luvemanates@gmail.com>
2024-01-25 09:53:10 -0800

    added more styling and fixed a button and link issue (#2)

--- app/helpers/ticket_comments_helper.rb
+++ app/helpers/ticket_comments_helper.rb
@@ -1,2 +1,9 @@
 module TicketCommentsHelper
+  def comment_color(current_ticket_count)
+    current_ticket_count = 1 unless current_ticket_count 
+    color_selector = current_ticket_count % 2 
+    ticket_colors = ["white", "gray"] 
+    ticket_color = ticket_colors[color_selector] 
+    return ticket_color
+  end
 end
