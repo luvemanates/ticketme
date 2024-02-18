b4dc82f747a62df6e5ab79b8de951a4ae80f70e6
luvemanates <luvemanates@gmail.com>
2024-02-18 11:22:28 -0800

    added long links for better search and display

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,8 @@ class Ticket < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
   has_many :ticket_comments
+
+  def to_param
+    self.id.to_s + '-' + self.ticket_to + '-' + self.description.parameterize
+  end
 end
